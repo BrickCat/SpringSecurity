b530898315c707e82d7ae7d31480f1f99094d043
搬砖猫 <549595297@qq.com>
2017-05-09 23:03:45 +0800

    将DispatcherServlet 映射到“/”

--- src/main/java/com/brickcat/WebConfigInitializer.java
+++ src/main/java/com/brickcat/WebConfigInitializer.java
@@ -6,15 +6,19 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
  * Created by Seven on 17/5/9.
  */
 public class WebConfigInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+    @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[0];
     }
 
+    @Override
     protected Class<?>[] getServletConfigClasses() {
         return new Class[0];
     }
 
+    //将DispatcherServlet 映射到“/”
+    @Override
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[] {"/"};
     }
 }
