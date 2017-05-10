98bfd008eb6332177618b4a4ccb905d7c0f18733
BrickCat <549595297@qq.com>
2017-05-10 17:19:08 +0800

    配置WebConfig

--- src/main/java/com/brickcat/config/WebConfig.java
+++ src/main/java/com/brickcat/config/WebConfig.java
@@ -1,7 +1,14 @@
 package com.brickcat.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 /**
  * Created by 860117030 on 2017/5/10.
  */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.brickcat")
 public class WebConfig {
 }
